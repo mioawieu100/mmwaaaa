d6812d234a92db42062b3ac01cc7f82cfd2c82fb
mioawieu100 <56777554+mioawieu100@users.noreply.github.com>
2019-10-20 06:32:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,110 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-console.log("3bker");
-var prefix = ".";
-client.on("message", message => {
+const discord = require('discord.js');
+const splashy = new discord.Client();
+const config = require('./config.json');
+let prefix = config.prefix;
 
-            if (message.content.startsWith(prefix + "la")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
+splashy.on('ready', () => {
+    console.log(`Splashy is online in ${splashy.guilds.size} Discord Server`);
+    let statuses = [
+        `s.help`,
+        `Most advanced Rainbow Role Bot`,
+        `Splashy Is in ${splashy.guilds.size} discord servers!`
+  
+    ]
+    setInterval(function(){
+      let GAMES = statuses[Math.floor(Math.random() * statuses.length)];
+      splashy.user.setPresence({ game: { name: GAMES }, type: 0, status: 'dnd' });
+    }, 10000)
 });
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : darkxx`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : black ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`!bc |mahmod AR |server`,"http://twitch.tv/Death Shop")
-client.user.setStatus("dnd")
+
+splashy.on('message', msg => {
+
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+    const args = msg.content.slice(prefix.length).split(' ');
+    const cmd = args.shift().toLowerCase();
+    const Role = args.join(" ");
+    const random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    const rRole = msg.guild.roles.find(role => role.name === Role);
+
+
+
+
+    function SplashyRianbow() {
+        let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+        let rRole = msg.guild.roles.find(role => role.name === Role);
+        if (!Role) return msg.channel.send('Please specify a role');
+        if (!rRole) return msg.channel.send('Cant find the specified role');
+        rRole.edit({color: random}).catch(e => {
+        })
+    };
+
+    if(cmd === 'rainbow'){
+        if (!Role) return msg.channel.send('Please specify a role');
+        if (!rRole) return msg.channel.send('Cant find the specified role');
+            setInterval(() => { SplashyRianbow(); }, config.Interval)
+            msg.channel.send({
+                embed: {
+                    color: msg.member.displayColor,
+                    author: { name: "Splashy Support Discord Server", url: 'https://discord.gg/HRh4jyW', icon_url: msg.guild.iconURL  },
+                    description: `I have enabled color changing role for ***${Role}***`
+                }
+            })
+    }
+    if(cmd === 'help'){
+        msg.author.send({
+            embed: {
+                color: 0x41e81b,
+                author: { name: "Splashy Support Discord Server.", url: 'https://discord.gg/HRh4jyW', icon_url: msg.guild.iconURL },
+                description: `${prefix}rainbow <role>\n${prefix}help - Shows this command...`
+            }
+        })
+    }
+
+    });
+
+splashy.on('guildCreate', guild => {
+console.log(`New guild joined ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members`);
+let channelID;
+    let channels = guild.channels;
+    channelLoop:
+    for (let c of channels) {
+        let channelType = c[1].type;
+        if (channelType === "text") {
+            channelID = c[0];
+            break channelLoop;
+        }
+    }
+    let channel = splashy.channels.get(guild.systemChannelID || channelID);
+    channel.send({
+        embed: {
+            color: 0x41e81b,
+            author: { name: "Splashy Support Discord Server." },
+            description: 'Bot Infomation.',
+            fields: [{
+                name: "Thank you for adding me to your server",
+                value: "From Splashy Developers."
+            },
+            {
+                name: "Support Discord Server:",
+                value: '[HERE](https://discord.gg/HRh4jyW)',
+                inline: true
+            }, 
+            {
+                name: "Bot Prefix:",
+                value: config.prefix,
+                inline: true
+            },
+            {
+                name: "Developer:",
+                value: "Jinx#4395",
+                inline: true
+            }]
+        }
+    });
+});
+splashy.on('guildDelete', guild => {
+console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
 });
 
-client.login(process.env.BOT_TOKEN);
+splashy.login(config.token);
